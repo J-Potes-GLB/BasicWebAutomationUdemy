689fcaf80adca03b51b990f293c22be3e39126fe
J-Potes-GLB <juan.potes@globant.com>
2024-03-14 18:15:37 -0500

    Added locators on the LoginPage class

--- src/test/java/com/herokuapp/theinternet/pages/LoginPage.java
+++ src/test/java/com/herokuapp/theinternet/pages/LoginPage.java
@@ -2,8 +2,24 @@ package com.herokuapp.theinternet.pages;
 
 import com.herokuapp.theinternet.utils.BasePage;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 public class LoginPage extends BasePage {
+    @FindBy(id = "username")
+    private WebElement usernameInput;
+
+    @FindBy(name = "password")
+    private WebElement passwordInput;
+
+    @FindBy(css = "button.radius")
+    private WebElement loginButton;
+
+    @FindBy(className = "button secondary radius")
+    private WebElement logoutButton;
+
+    @FindBy(id = "flash")
+    private WebElement successfulLoginMessage;
 
     public LoginPage(WebDriver driver) {
         super(driver);
